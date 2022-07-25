36e34ddae91ba07580fa85af39c5be70654688f1
emran <omranshahbazi1998@gmail.com>
2022-07-25 10:41:45 +0430

    A commit for resolved issues ( #6, #5, #4, #3, #2)

--- Maktab HWs/Emran_Shahbazi_hw3_maktab78/HW3_1.py
+++ Maktab HWs/Emran_Shahbazi_hw3_maktab78/HW3_1.py
@@ -6,42 +6,39 @@ class BirthDay:
     
     def __init__(self , year , month , day , hour):
         
-        self.y = year
-        self.m = month
-        self.d = day
-        self.h = hour
+        self.year = year
+        self.month = month
+        self.day = day
+        self.hour = hour
         
     def age(self):
         
-        y1 = s('%Y' , l())
-        m1 = s('%m' , l())
-        #d1 = s('%d' , l())
-        #h1 = s('%H' , l())
+        year_now = s('%Y' , l())
+        month_now = s('%m' , l())
         
-        if m1 >= self.m : 
-            y2 = y1 - self.y
+        if int(month_now) >= self.month: 
+            year_diff = year_now - self.year
 
         else:
-            y2 = y1 - self.y - 1
+            year_diff = int(year_now) - self.year - 1
             
-        return (f'Age is {y2} years , or {y2*8760} hours')
+        return (f'Age is {year_diff} years , or {year_diff*8760} hours')
     
     def to_birthday(self):
         
-        m1 = int(s('%m' , l()))
-        d1 = int(s('%d' , l()))
-        d2 = self.d - d1    
-        m2 = self.m - m1
-        if self.m >= m1:
+        month_now = int(s('%m' , l()))
+        day_now = int(s('%d' , l()))
+        day_diff = self.day - day_now    
+        month_diff = self.month - month_now
+        if self.month >= month_now:
             
-            if self.d >= d1:
+            if self.day >= day_now:
                 
-                return (f'{m2} months and {d2} days left to birthday')
+                return (f'{month_diff} months and {day_diff} days left to birthday')
             
             else:
-                return (f'{m2 - 1} months and {30 + d2} days left to birthday')
+                return (f'{month_diff - 1} months and {30 + day_diff} days left to birthday')
             
         else:
-            return (f'{12 + m2 - 1} months and {30 + d2} days left to birthday')
-                
-        
+            return (f'{12 + month_diff - 1} months and {30 + day_diff} days left to birthday')
+  
--- Maktab HWs/Emran_Shahbazi_hw3_maktab78/HW3_2.py
+++ Maktab HWs/Emran_Shahbazi_hw3_maktab78/HW3_2.py
@@ -1,23 +1,25 @@
 class BankAccount:
     
+    _min_balance = 0
+    
     def __init__(self , account_owner_name , balance , minimum_balance):
         
         self.account_owner_name = account_owner_name
         self.balance = balance
-        self.minimum_balance = minimum_balance
+        self.__clas__._minimum_balance = minimum_balance
         
 
     def deposit(self  , amount):
         
-        self.b -= amount
+        self.b += amount
         
     def withdraw(self , amount):
         
-        self.b += amount
+        self.b -= amount
         
     def transfer(self , amount , transfer_bank_account):
         
-        if self.balance - amount < self.minimum_balance:
+        if self.balance - amount < self.__class__._minimum_balance:
             return('This transaction is not possible !!')
         
         else:
